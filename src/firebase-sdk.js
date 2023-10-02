a235757339ee84611e36cc30795afda50b92fd92
mahmoud-khalil8 <mahmoudkhalil8.g@gmail.com>
2023-10-02 20:56:15 +0300

    update file structure

---
+++ src/firebase-sdk.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9gzl958G4ucgB1zdOSuDz-K4ZbqRoM0I",
+  authDomain: "fir-js-57cf1.firebaseapp.com",
+  projectId: "fir-js-57cf1",
+  storageBucket: "fir-js-57cf1.appspot.com",
+  messagingSenderId: "164649600362",
+  appId: "1:164649600362:web:1d3ee8887f4218260c9113"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
